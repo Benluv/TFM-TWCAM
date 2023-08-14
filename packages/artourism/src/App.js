82b8d501365bd527dc8794001af0e2000bf785c2
Benluv <luongov13@gmail.com>
2023-08-14 19:06:21 +0200

    initial commit

---
+++ packages/artourism/src/App.js
@@ -0,0 +1,41 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import XrHitCubeContainer from "./components/xr-hit-cube/XrHitCubeContainer";
+import Test from './components/Text';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<XrHitCubeContainer />} />
+      <Route path="/test" element={<Test />} />
+    </Routes>
+  )
+}
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>Hello!</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ packages/artourism/src/components/Text.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+    return (
+        <div>
+        <p>https://r3f-fiber-draft.vercel.app/cube</p>
+        </div>
+    );
+}
+
+export default Test;
