f35d14e2ab275eb52e7b807ccceeb5c89a7ddf0f
Benluv <luongov13@gmail.com>
2023-08-25 19:07:03 +0200

    added the not found page when accesing a page that is not registered

---
+++ packages/artourism/src/components/xr-gallery/notFound/NotFound.jsx
@@ -0,0 +1,14 @@
+// NotFound.js
+import React from 'react';
+import './Styles.css';
+
+const NotFound = () => {
+  return (
+    <div className="not-found-container">
+      <h1>404</h1>
+      <p>Oops! Page not found.</p>
+    </div>
+  );
+};
+
+export default NotFound;
