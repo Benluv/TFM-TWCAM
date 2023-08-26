dd822ad38b1964e6da7ed17fd94e4947088579d6
Benluv <luongov13@gmail.com>
2023-08-26 16:00:39 +0200

    removed optionalFeatures and domOverlay with its states from XRGalleryContainer to see if it works now

--- packages/artourism/src/components/xr-gallery/XrGalleryContainer.jsx
+++ packages/artourism/src/components/xr-gallery/XrGalleryContainer.jsx
@@ -1,12 +1,9 @@
 import { Canvas } from "@react-three/fiber";
 import { ARButton, XR } from "@react-three/xr";
-import { useCallback, useState } from "react";
 import { CharacterAnimationsProvider } from "../../contexts/CharacterAnimations";
-import Interface from "./Interface";
 import XrGallery from "./XrGallery";
 
 const XrGalleryContainer = () => {
-  const [overlayContent, setOverlayContent] = useState(null);
 
   return (
     <>
@@ -15,8 +12,6 @@ const XrGalleryContainer = () => {
           className="xr-button"
           sessionInit={{
             requiredFeatures: ["hit-test"],
-            optionalFeatures: ["dom-overlay"],
-            domOverlay: { root: overlayContent },
           }}
         />
         <Canvas>
